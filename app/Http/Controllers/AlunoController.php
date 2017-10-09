b6ef30c46dbbf1da6b2e3815acaa7aeb76b5dfb6
lucasmknopf <lucasmknopf@gmail.com>
2017-10-09 00:23:48 -0300

    inicio controllers

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
+{
+    public function index(){
+        return "olaaaaaaaaa!".
+            "<br>".
+            "sou o index";
+
+    }
+}
---
+++ app/Http/Controllers/Turma_alunoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Turma_alunoController extends Controller
+{
+    //
+}
